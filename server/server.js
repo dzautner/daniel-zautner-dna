35284771d09e677e838db2858fcbf8a73f83a7e5
Matt Bengston <mattb@unity3d.com>
2018-09-12 20:21:53 -0400

    Added dynamic port

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 const DANIEL_DNA_FILE = 'genome_Daniel_Zautner_v4_Full_20180401133702.txt';
+const PORT = process.env.PORT || 8080;
 
 let dna = '';
 
@@ -15,6 +16,6 @@ const dnaFile = path.join(__dirname, '../', DANIEL_DNA_FILE);
 
 fs.readFile(dnaFile, function(err, data) {
   dna = data.toString('utf8');
-  app.listen(8000);
-  console.log('Running daniel on port 8000');
+  app.listen(PORT);
+  console.log('Running daniel on port ' + PORT);
 });
